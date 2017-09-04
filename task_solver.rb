5e150ee72b4503bb4db80c26c0d02f5ee43d1703
Volodymyr Kulyk <dev.volodymyr@gmail.om>
2017-09-04 12:24:45 +0300

    Added  task solver for abramov tasks

---
+++ task_solver.rb
@@ -0,0 +1,68 @@
+
+module MyModule
+  def t_331b(n)
+    answers = []
+    n = n.to_i
+    (0..Math.sqrt(n)).each do |x|
+      (0..Math.sqrt(n)).each do |y|
+        (0..Math.sqrt(n)).each do |z|
+          sum = 2**x + 2**y + 2**z
+          answers << Hash[:x, x, :y, y, :z, z, :sum, sum] if sum == n
+        end
+      end
+    end
+    answers
+  end
+end
+
+# Second module
+module SecondModule
+  def t_108(n)
+    n = n.to_i
+    pow = 0
+    sum = 1
+    while sum <= n
+      pow += 1
+      sum = 2**pow
+    end
+    puts "(2^#{pow} = #{sum}) > #{n}"
+  end
+end
+
+# Abramov tasks solver
+class MyClass
+  include MyModule
+  include MyModuleW
+  attr_accessor :modules, :tasks
+
+  def initialize
+    @tasks = []
+    @modules = MyClass.included_modules.reject do |item|
+      ['JSON::Ext::Generator::GeneratorMethods::Object',
+       'Kernel'].include?(item.to_s)
+    end
+    @modules.each do |mod|
+      @tasks << mod.public_instance_methods.select {|func| /t_\d+.{0,2}/ =~ func.to_s}.map {|x| x.to_s.delete('t_')}
+    end
+  end
+
+  def start
+    puts 'Hey there! Here are some tasks that i can solve for you:'
+    puts @tasks.join(', ')
+    puts "Please, enter the name of the task you want to solve?
+    \nExample: 108, 91a and etc."
+    task_number = gets.chomp
+    return if task_number == 'exit' || task_number.empty?
+    return "We can't solve such a complex task :D" unless @tasks.find { |x| x.include?(task_number) }
+    puts "To solve this task, you need to pass
+          #{method("t_#{task_number}").arity} arguments, separated by comma:"
+    arguments = gets.chomp.split(',')
+    puts send("t_#{task_number}", *arguments)
+    puts "We hope you are happy with our solution!
+          \nPsss.. Wanna solve more tasks? (yes/no)"
+    gets.chomp == 'yes' ? start : return
+  end
+end
+
+abramov = MyClass.new
+abramov.start
